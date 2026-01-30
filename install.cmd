133020e9ed6e1c021de1830ffd8a7e38eb0a9342
Roman Karkhachev <karkhachev@cortona3d.com>
2026-01-30 09:57:45 -0800

    copy_admin.cmd - script for copying files install.cmd - all other things

--- install.cmd
+++ install.cmd
@@ -4,7 +4,7 @@ set TC_DATA=c:\Siemens\Teamcenter\tc_data
 call %TC_DATA%\tc_profilevars
 import_uiconfig -u=infodba -p=infodba -g=dba -file=ui_config_awtcdispatcher.xml -action=merge
 aws2_install_tilecollections -u=infodba -p=infodba -g=dba -mode=add -file=DispatcherConsole_Tiles_install.xml
-robocopy "aws2" "%TC_ROOT%\aws2" /E /R:0 /W:0
+REM robocopy "aws2" "%TC_ROOT%\aws2" /E /R:0 /W:0
 cd %TC_ROOT%\aws2\stage\src\solution
 powershell -NoProfile -Command "$j=Get-Content 'kit.json' -Raw | ConvertFrom-Json; if ($j.modules -notcontains 'dispatcherconsole') { $j.modules=@('dispatcherconsole')+$j.modules }; $utf8NoBom = New-Object System.Text.UTF8Encoding($false); [System.IO.File]::WriteAllText('kit.json', ($j | ConvertTo-Json -Depth 20), $utf8NoBom)"
 %TC_ROOT%\aws2\stage\awbuild.cmd
